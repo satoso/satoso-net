03fea0a3743e66481875600dba74626ca9cc2291
satoso <git.satoso@outlook.com>
2014-03-09 21:55:19 +0900

    update script fix2

--- update-site.sh
+++ update-site.sh
@@ -5,37 +5,19 @@ if [ ! "$1" ]; then
 fi
 
 bundle exec jekyll build \
+  && echo '--------' \
   && git add --all --verbose . \
+  && echo '--------' \
   && git commit -m "$1" --verbose \
+  && echo '--------' \
   && pushd _site \
+  && echo '--------' \
   && git add --all --verbose . \
+  && echo '--------' \
   && git commit -m "$1" --verbose \
+  && echo '--------' \
   && popd \
+  && echo '--------' \
   && git push --all --verbose origin
-exit 0
-
-jk_build="bundle exec jekyll build"
-git_add="git add --all --verbose ."
-git_commit="git commit --verbose -m '$1'"
-git_push="git push --all --verbose origin"
-
-echo $jk_build
-echo $git_commit
-$jk_build && $git_add && $git_commit \
-  && pushd _site && $git_add && $git_commit \
-  && popd \
-  && $git_push
-
-exit 0
-
-if $build; then
-  git add --all --verbose .
-  git commit -m $1
-  pushd _site
-  git add --all --verbose .
-  git commit -m $1
-  popd
-  git push --all --verbose origin
-fi
 exit 0
 
