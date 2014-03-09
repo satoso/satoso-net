0eb3eaebf0951e4860d581740148875bd06fa557
satoso <git.satoso@outlook.com>
2014-03-09 21:44:55 +0900

    diary url fix

---
+++ update-site.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+if [ ! "$1" ]; then
+  echo 'comment not specified'
+  exit 1
+fi
+
+bundle exec jekyll build \
+  && git add --all --verbose . \
+  && "git commit -m '$1' --verbose" \
+  && pushd _site \
+  && git add --all --verbose . \
+  && "git commit -m '$1' --verbose" \
+  && popd \
+  && git push --all --verbose origin
+
+exit 0
+
+jk_build="bundle exec jekyll build"
+git_add="git add --all --verbose ."
+git_commit="git commit -m --verbose '$1'"
+git_push="git push --all --verbose origin"
+
+echo $jk_build
+echo $git_commit
+exit 0
+$jk_build && $git_add && $git_commit && pushd _site && $git_add && $git_commit && popd && $git_push
+
+if $build; then
+  git add --all --verbose .
+  git commit -m $1
+  pushd _site
+  git add --all --verbose .
+  git commit -m $1
+  popd
+  git push --all --verbose origin
+fi
+exit 0
+
