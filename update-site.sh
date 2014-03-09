17d95112b5a4bfd83ffb146903e3df0c3890cade
satoso <git.satoso@outlook.com>
2014-03-09 21:52:52 +0900

    update script fix

--- update-site.sh
+++ update-site.sh
@@ -6,24 +6,27 @@ fi
 
 bundle exec jekyll build \
   && git add --all --verbose . \
-  && "git commit -m '$1' --verbose" \
+  && git commit -m "$1" --verbose \
   && pushd _site \
   && git add --all --verbose . \
-  && "git commit -m '$1' --verbose" \
+  && git commit -m "$1" --verbose \
   && popd \
   && git push --all --verbose origin
-
 exit 0
 
 jk_build="bundle exec jekyll build"
 git_add="git add --all --verbose ."
-git_commit="git commit -m --verbose '$1'"
+git_commit="git commit --verbose -m '$1'"
 git_push="git push --all --verbose origin"
 
 echo $jk_build
 echo $git_commit
+$jk_build && $git_add && $git_commit \
+  && pushd _site && $git_add && $git_commit \
+  && popd \
+  && $git_push
+
 exit 0
-$jk_build && $git_add && $git_commit && pushd _site && $git_add && $git_commit && popd && $git_push
 
 if $build; then
   git add --all --verbose .
